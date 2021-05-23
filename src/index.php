7e9265c5529f8ead5b486c8f846231500a77ae94
Lucian Diodiu <lucian.diodiu@zitec.com>
2021-05-23 15:03:17 +0300

    added project name and style it

--- src/index.php
+++ src/index.php
@@ -1,13 +1,19 @@
 <html lang="ro">
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
         <title>Warehouse</title>
     </head>
-<body style="background-image: url('files/Images/warehouse.jpg');">
+<body style="background-color: #ab3428">
     <ul class="nav nav-tabs justify-content-center">
         <li class="active ml-3"><a class="btn btn-primary" href="index.php">Acasa</a></li>
         <li class="ml-3"><a class="btn btn-primary" href="Views/Risk.php">Secvente de accident</a></li>
         <li class="ml-3"><a class="btn btn-primary" href="Views/ProtectionZones.php">Zone de protectie</a></li>
     </ul>
+
+    <div class="logo">
+        <h1>RESICEX</h1>
+        <h2>Risc de explozie specific infrastructurilor critice din domeniul explozivilor de uz civil</h2>
+    </div>
 </body>
 </html>
